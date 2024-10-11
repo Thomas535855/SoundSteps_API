d7fbb03f46cb46b2ca28c5c6fdf398ca12932f86
Thomas Albregts <535855@student.fontys.nl>
2024-10-11 15:36:37 +0200

    program.cs changes

--- SoundSteps.API/Program.cs
+++ SoundSteps.API/Program.cs
@@ -1,6 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using SoundSteps.DAL;
 using SoundSteps.DAL.DALs;
 using SoundSteps.Interface.Interfaces;
@@ -8,26 +6,29 @@ using SoundSteps.Logic.Containers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Get the connection string from configuration
 var connectionString = builder.Configuration.GetConnectionString("SoundStepsDb");
 
-// Add services to the container
+
+// Add services to the container.
 builder.Services.AddScoped<UserContainer>();
 builder.Services.AddScoped<IUserDAL, UserDAL>();
-
-// Register the DbContext and configure it to use SQL Server
-builder.Services.AddDbContext<SoundStepsDbContext>(options =>
-    options.UseSqlServer(connectionString, b => b.MigrationsAssembly("SoundSteps.DAL"))
-);
-
-// Add controllers
+builder.Services.AddDbContext<SoundStepsDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddControllers();
+
+
+
+builder.Services.AddDbContext<SoundStepsDbContext>(options =>
+    options.UseSqlServer(
+        connectionString,
+        b => b.MigrationsAssembly("SoundSteps.DAL") // Specify the DAL project for migrations
+    )
+);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// CORS policy
+//CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowVueApp",
@@ -43,7 +44,7 @@ var app = builder.Build();
 
 app.UseCors("AllowVueApp");
 
-// Configure the HTTP request pipeline
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -51,7 +52,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
