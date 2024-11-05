3eef9b9c2746d76b682eaaedee83795d6ad31762
Thomas Albregts <535855@student.fontys.nl>
2024-11-05 15:40:06 +0100

    Added unit tests for usercontainer

--- SoundSteps.Test/UnitTests/Containers/UserContainerTest.cs
+++ SoundSteps.Test/UnitTests/Containers/UserContainerTest.cs
@@ -22,33 +22,93 @@ namespace SoundSteps.Test.UnitTests.Containers
         }
 
         [TestMethod]
-        public async Task AddUser_SuccesfullyAddUser()
+        public async Task Add_ShouldCallAddUserOnce()
         {
-            UserDTO userDTO = new UserDTO
-            {
-                UserId = 1,
-                Username = "TestUser",
-                Email = "TestEmail",
-                Password = "TestPassword",
-                SkillLevel = 1
-            };
-
-            _userDAL.Setup(dal => dal.AddUser(userDTO)).Returns(Task.CompletedTask);
+            // Arrange
+            var userDto = new UserDTO { UserId = 1, Username = "Test User" };
 
             // Act
-            await _userContainer.Add(userDTO);
+            await _userContainer.Add(userDto);
 
             // Assert
-            _userDAL.Verify(dal => dal.AddUser(It.Is<UserDTO>(u =>
-                u.UserId == 1 &&
-                u.Username == "TestUser" &&
-                u.Email == "TestEmail" &&
-                u.Password == "TestPassword" &&
-                u.SkillLevel == 1)),
-                Times.Once,
-                "Expected AddUser to be called once with the correct UserDTO.");
+            _userDAL.Verify(dal => dal.AddUser(userDto), Times.Once, "AddUser should be called once with the given DTO.");
         }
 
-        
+        [TestMethod]
+        public async Task Delete_ShouldCallDeleteUserOnce()
+        {
+            // Arrange
+            int userId = 1;
+
+            // Act
+            await _userContainer.Delete(userId);
+
+            // Assert
+            _userDAL.Verify(dal => dal.DeleteUser(userId), Times.Once, "DeleteUser should be called once with the given user ID.");
+        }
+
+        [TestMethod]
+        public async Task Update_ShouldCallUpdateUserOnce()
+        {
+            // Arrange
+            var userDto = new UserDTO { UserId = 1, Username = "Updated User" };
+
+            // Act
+            await _userContainer.Update(userDto);
+
+            // Assert
+            _userDAL.Verify(dal => dal.UpdateUser(userDto), Times.Once, "UpdateUser should be called once with the given DTO.");
+        }
+
+        [TestMethod]
+        public async Task GetAll_ShouldReturnListOfUserDTOs()
+        {
+            // Arrange
+            var users = new List<UserDTO>
+            {
+                new UserDTO { UserId = 1, Username = "User 1" },
+                new UserDTO { UserId = 2, Username = "User 2" }
+            };
+            _userDAL.Setup(dal => dal.GetAllUsers()).ReturnsAsync(users);
+
+            // Act
+            var result = await _userContainer.GetAll();
+
+            // Assert
+            Assert.AreEqual(users, result, "GetAll should return the list of users provided by the DAL.");
+            _userDAL.Verify(dal => dal.GetAllUsers(), Times.Once, "GetAllUsers should be called once.");
+        }
+
+        [TestMethod]
+        public async Task GetById_ShouldReturnCorrectUserDTO()
+        {
+            // Arrange
+            int userId = 1;
+            var user = new UserDTO { UserId = userId, Username = "Test User" };
+            _userDAL.Setup(dal => dal.GetUserById(userId)).ReturnsAsync(user);
+
+            // Act
+            var result = await _userContainer.GetById(userId);
+
+            // Assert
+            Assert.AreEqual(user, result, "GetById should return the user DTO provided by the DAL.");
+            _userDAL.Verify(dal => dal.GetUserById(userId), Times.Once, "GetUserById should be called once with the given user ID.");
+        }
+
+        [TestMethod]
+        public async Task GetById_ShouldReturnNullIfUserNotFound()
+        {
+            // Arrange
+            int userId = 1;
+            _userDAL.Setup(dal => dal.GetUserById(userId)).ReturnsAsync((UserDTO?)null);
+
+            // Act
+            var result = await _userContainer.GetById(userId);
+
+            // Assert
+            Assert.IsNull(result, "GetById should return null if the user is not found.");
+            _userDAL.Verify(dal => dal.GetUserById(userId), Times.Once, "GetUserById should be called once with the given user ID.");
+        }
+
     }
 }
